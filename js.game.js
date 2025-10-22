d45612f437cad578281150e21855697aa52fef4e
Soliko611 <soli.kolet@gmail.com>
2025-10-22 20:58:59 +0300

    Second 21:00

--- js.game.js
+++ js.game.js
@@ -4,12 +4,6 @@
 
 var gBoard
 
-var Cell = {
-    minesAroundCount: 4,
-    isRevealed: false,
-    isMine: false,
-    isMarked: false,
-}
 
 const gLevel = {
     SIZE: 4,
@@ -27,12 +21,18 @@ function onInit() {
     renderBoard(gBoard)
 }
 function createCell() {
-    return {
+    var cellObject = {
         minesAroundCount: 0,
         isRevealed: false,
         isMine: false,
         isMarked: false,
     }
+    return cellObject
+}
+function setDifficulty(size) {
+    gLevel.SIZE = size
+    onInit()
+
 }
 
 function buildBoard() {
@@ -42,8 +42,11 @@ function buildBoard() {
         for (var j = 0; j < gLevel.SIZE; j++) {
             board[i][j] = createCell()
         }
-        Cell[1][2] = !ismine
-        Cell[2][2] = !ismine
+
+    }
+    if (gLevel.SIZE > 2) {
+        board[1][2].isMine = true
+        board[2][2].isMine = true
     }
     return board
 
@@ -51,16 +54,21 @@ function buildBoard() {
 function renderBoard(board) {
     const elBoard = document.querySelector('.board')
     var strHtml = ''
-    for (var i = 0; i < gBoard.length; i++) {
+    for (var i = 0; i < board.length; i++) {
         strHtml += `\n<tr>`
 
-        for (var j = 0; j < gBoard[i].length; j++) {
-            const strClassName = gBoard[i][j]
+        for (var j = 0; j < board[i].length; j++) {
+            const cell = board[i][j]
+            var Name = `cell`
+            if (cell.isMine) Name += ` mine`
+
+            const cellContent = cell.isMine ? `💣` : countNeighbors(board, i, j)
             strHtml += `\n\t<td 
                 data-i="${i}" data-j="${j}"
-                onclick="onCellClicked(${i}, ${j}, this)"
-                class="${strClassName}">${gBoard[i][j]}</td>`
+                onclick="onCellClicked(this,${i}, ${j})"
+                class="${Name}">${cellContent}</td>`
         }
+
         strHtml += `\n</tr>`
     }
     elBoard.innerHTML = strHtml
@@ -68,13 +76,30 @@ function renderBoard(board) {
 }
 
 function setMinesNegsCount(board) {
+    for (var i = 0; i < board.length; i++) {
+        for (var j; j < board.length; j++) {
+            board[i][j].minesAroundCount = countNeighbors(board,i,j)
 
+        }
+    }
 }
-
 
 function onCellClicked(elCell, i, j) {
+    var cell = gBoardboard[i][j]
+    if(cell.isRevealed)return
+    cell.isRevealed = true
 
+    if(cell.isMine){
+        elCell.innerHTML ="💣"
+        console.log(`game over`)
+        return
+        
+    }
+    var minesCount = countNeighbors(gBoard,i,j)
+    elCell.innerHTML = minesCount || ''
 }
+
+
 
 function onCellMarked(elCell, i, j) {
 
@@ -86,3 +111,16 @@ function checkGameOver() {
 function expandReveal(board, elCell, i, j) {
 
 }
+
+//will use later for cancel left click on table
+// const myTable = document.getElementById('myTable');
+
+//     // Add an event listener for the 'click' event
+//     myTable.addEventListener('click', function(event) {
+//       // Prevent the default action (e.g., navigating to a link)
+//       event.preventDefault();
+//       // Optionally, you can also stop the event from bubbling up to other elements
+//       event.stopPropagation();
+//       // You can also return false
+//       return false;
+//     });
--- js.util.js
+++ js.util.js
@@ -10,3 +10,17 @@ function renderCell(location, value) {
 function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min
 }
+
+function countNeighbors(board, celli, cellj) { //minesAroundCount
+     var count = 0
+    for (var i = celli - 1; i <= celli + 1; i++) {
+        if (i < 0 || i >= board.length) continue
+
+        for (var j = cellj - 1; j <= cellj + 1; j++) {
+            if (j < 0 || j >= board[i].length) continue
+            if (i === celli && j === cellj) continue
+            if (board[i][j].isMine) count++
+        }
+    }
+    return count
+}
